abfee4bd9dc2448ce3726c679ff27173158fe7e4
Bacon <69711892+Bacon42069@users.noreply.github.com>
2022-11-23 12:50:25 +0100

    Updated Backpacking Script Finally Working to be faster

--- Backpacking Script Finally Working.lua
+++ Backpacking Script Finally Working.lua
@@ -11,12 +11,12 @@ if(getgenv().kuefg834rjiy983450==nil)then game:GetService("Players").LocalPlayer
 spawn(function()loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))();end);
 
 -- Funcs
-function getClosestChest(model)
+function getClosestChest(model,prevChest)
     local range=9e9;
     local chest=nil;
     xpcall(function()
         for _,cChest in pairs(model:GetChildren())do 
-            if(cChest:IsA("Model"))and(cChest:FindFirstChild("Hitbox")~=nil)and(cChest.Name=="Chest")and(cChest:FindFirstChild("Open")~=nil)and(cChest:FindFirstChild("Open").Value==false)then 
+            if(cChest.Name=="Chest")and(cChest:IsA("Model"))and(cChest~=prevChest)and(cChest:FindFirstChild("Hitbox")~=nil)and(cChest.Hitbox:FindFirstChild("TouchInterest")~=nil)and(cChest:FindFirstChild("Open")~=nil)and(cChest:FindFirstChild("Open").Value==false)then 
                 local dist=(game:GetService("Players")["LocalPlayer"]["Character"]["HumanoidRootPart"]["Position"]-cChest["Hitbox"]["Position"])["Magnitude"];
                 if(dist<range)then 
                     range=dist;
@@ -53,32 +53,36 @@ end);
 b:Toggle("Auto Open Chests",function(a)
     getgenv().OpenChests=a;
     print(a);
+    local done=false;
     spawn(function()
-        pcall(function()
-            local oldCF=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame;
-            local completed=false
-            while(getgenv().OpenChests==true)do 
-                game:GetService("Workspace").CurrentCamera.CameraSubject=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart;
-                for _,a in pairs(game:GetService("Players").LocalPlayer.Character.Humanoid:GetPlayingAnimationTracks())do a:Stop(0);end;
+        local oldCF;pcall(function()oldCF=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame;end);
+        local completed=false;
+        local prevChest=nil;
+        while(getgenv().OpenChests==true)and(game:GetService("RunService").Stepped:Wait())do 
+            pcall(function()
                 completed=false;
-                local chest=getClosestChest(game:GetService("Workspace").Scenery.Chests.ChestObjects);
-                local info=TweenInfo.new((game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Position-chest.Hitbox.Position).Magnitude/70,Enum.EasingStyle.Linear);
+                local chest=getClosestChest(game:GetService("Workspace").Scenery.Chests.ChestObjects,prevChest);
+                prevChest=chest;
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=chest.Hitbox.CFrame*CFrame.new(0,0,-60);
+                local info=TweenInfo.new(0.5,Enum.EasingStyle.Linear);
                 local a=game:GetService("TweenService"):Create(game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,info,{CFrame=chest.Hitbox.CFrame});
                 a.Completed:Connect(function()completed=true;end);
                 a:Play();
                 while(completed==false)do wait();end;
-                wait(2);
-            end;
-            while(completed==false)do wait();end;
+                wait(0.6);
+            end);
+        end;
+        pcall(function()
+            done=true;
+            wait();
             game:GetService("Players").LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp,true);
             game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp);
-            game:GetService("Workspace").CurrentCamera.CameraSubject=game:GetService("Players").LocalPlayer.Character.Humanoid;
             game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=oldCF;
         end);
     end);
 
     spawn(function()
-        while(getgenv().OpenChests==true)and(game:GetService("RunService").Stepped:Wait())do 
+        while((getgenv().OpenChests==true)or(done==false))and(game:GetService("RunService").Stepped:Wait())do 
             pcall(function()
                 if(game:GetService("Players").LocalPlayer.Character.Humanoid.Sit==false)then 
                     game:GetService("Players").LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp,false);
@@ -87,6 +91,7 @@ b:Toggle("Auto Open Chests",function(a)
                     game:GetService("Players").LocalPlayer.Character.Humanoid:SetStateEnabled(Enum.HumanoidStateType.GettingUp,true);
 			        game:GetService("Players").LocalPlayer.Character.Humanoid:ChangeState(Enum.HumanoidStateType.GettingUp);
                 end;
+                for _,a in pairs(game:GetService("Players").LocalPlayer.Character.Humanoid:GetPlayingAnimationTracks())do a:Stop(0);end;
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity=Vector3.new(0,0,0);
             end);
         end;
