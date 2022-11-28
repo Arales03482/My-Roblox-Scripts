2c95e4e8f6f3dd88abf0e0a17649b290187f909f
Bacon <69711892+Bacon42069@users.noreply.github.com>
2022-11-28 15:17:47 +0100

    Update makeId_example.lua

--- Bacon Utils/makeId_example.lua
+++ Bacon Utils/makeId_example.lua
@@ -1,1 +1,7 @@
+print(game:HttpGet("https://raw.githubusercontent.com/Bacon42069/My-Roblox-Scripts/main/Bacon%20Utils/src.lua",true));
 
+local amount=48;
+local chars={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","9"};
+local charsUntilSeperator=8;
+local seperator="-";
+print(getgenv().baconUtils.makeId(amount,chars,charsUntilSeperator,seperator));
