38a09622153bed77d0d89049dbb53bdd3c60236b
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-02-15 23:37:47 +0100

    Update client.lua

--- LagVisualizer/client.lua
+++ LagVisualizer/client.lua
@@ -1,3 +1,4 @@
-getgenv().host="ws://localhost:2344/";
+getgenv().hostuser="ARealPerson29";
+getgenv().hosturl="ws://localhost:2344/";
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Bacon42069/My-Roblox-Scripts/main/LagVisualizer/src.lua",true))();
