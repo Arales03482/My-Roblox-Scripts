47ac532ce9be068f3b01e19d06d790aa69706dde
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-02-16 21:03:32 +0100

    Update client.lua

--- LagVisualizer/client.lua
+++ LagVisualizer/client.lua
@@ -1,4 +1,5 @@
-getgenv().hostuser="ARealPerson29";
-getgenv().hosturl="ws://localhost:2344/";
+getgenv().hostuser="ARealPerson29"; -- username of the host account set this to "nil" or nil to set all accounts to display each others lag
+getgenv().hosturl="ws://192.168.1.177:2344/"; -- host url obv
+getgenv().vizfreq=0.1; -- how frequent changes should be sent to the server
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Bacon42069/My-Roblox-Scripts/main/LagVisualizer/src.lua",true))();
