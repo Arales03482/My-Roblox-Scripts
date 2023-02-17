7a04a431757a511ea623d1c721a1f533b55853d9
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-02-17 19:48:44 +0100

    Update src.lua

--- LagVisualizer/src.lua
+++ LagVisualizer/src.lua
@@ -2,7 +2,6 @@ local socket;
 local currentUsers={};
 local lastdatas={};
 local data={};
-local items="{'HumanoidRootPart','Torso','Left Arm','Right Arm','Left Leg','Right Leg','Head'}";
 local game=game;
 
 --Anti AFK
@@ -10,6 +9,7 @@ if(getgenv().kuefg834rjiy983450==nil)then game:GetService("Players").LocalPlayer
 
 if(getgenv().hosturl==nil)then getgenv().hosturl="ws://192.168.1.177:2344/";end;
 if(getgenv().vizfreq==nil)then getgenv().vizfreq=0.1;end;
+if(getgenv().vizitems==nil)then getgenv().vizitems="{'HumanoidRootPart','Torso','Left Arm','Right Arm','Left Leg','Right Leg','Head'}";end;
 
 local socker=((Krnl~=nil)and(Krnl.WebSocket~=nil)and(Krnl.WebSocket.connect))or((syn~=nil)and(syn.websocket~=nil)and(syn.websocket.connect));
 
@@ -90,7 +90,7 @@ spawn(function()
                     lastdatas={};
                 elseif(spi[1]=="data")and(spi[2]=="lagviz")and(spi[3]==game:GetService("Players").LocalPlayer.Name)and(spi[4]~=game:GetService("Players").LocalPlayer.Name)then 
                     print(spi[6]);
-                    local ritems=toTable(spi[5],items);
+                    local ritems=toTable(spi[5],getgenv().vizitems);
                     local rdata=toTable(spi[6],{["HumanoidRootPart"]={Position=Vector3.new(0,0,0),Rotation=Vector3.new(0,0,0),Size=Vector3.new(2,2,1)}});
                     for _,item in pairs(ritems)do 
                         spawn(function()
@@ -124,16 +124,17 @@ if((getgenv().hostuser~=nil)and(getgenv().hostuser~=game:GetService("Players").L
         while(getgenv().testcdesync==true)do 
             xpcall(function()
                 if(socket~=nil)then 
-                    local ttbl=toTable(items,{'HumanoidRootPart','Torso','Left Arm','Right Arm','Left Leg','Right Leg','Head'});
+                    local ttbl=toTable(getgenv().vizitems,{'HumanoidRootPart','Torso','Left Arm','Right Arm','Left Leg','Right Leg','Head'});
                     for b,a in pairs(currentUsers)do 
                         if(a~=nil)and(a.Parent~=nil)and(a.Character~=nil)and(a.Name~=game:GetService("Players").LocalPlayer.Name)then 
                             local data="{";
-                            for _,part in pairs(a.Character:GetChildren())do 
-                                if(part:IsA("BasePart"))and(table.find(ttbl,part.Name))then 
+                            for _,item in pairs(ttbl)do 
+                                local part=a.Character:FindFirstChild(item);
+                                if(part~=nil)and(part:IsA("BasePart"))then 
                                     local pos=CFrame.new(0,0,0);
                                     local rot=CFrame.new(0,0,0);
                                     local sz=Vector3.new(2,2,1);pcall(function()pos=part.Position;rot=part.Rotation;sz=part.Size;end);
-                                    data=data.."['"..part.Name.."'] = {Position = Vector3.new("..tostring(pos).."), Rotation = Vector3.new("..tostring(rot).."), Size = Vector3.new("..tostring(sz)..")}, "
+                                    data=data.."['"..item.."'] = {Position = Vector3.new("..tostring(pos).."), Rotation = Vector3.new("..tostring(rot).."), Size = Vector3.new("..tostring(sz)..")}, "
                                 end;
                             end;
                             data=data:sub(1,data:len()-2);
@@ -141,11 +142,13 @@ if((getgenv().hostuser~=nil)and(getgenv().hostuser~=game:GetService("Players").L
                             local hrppos=CFrame.new(0,0,0);
                             local hrprot=CFrame.new(0,0,0);
                             local hrpsz=Vector3.new(2,2,1);pcall(function()hrppos=a.Character.HumanoidRootPart.Position;hrprot=a.Character.HumanoidRootPart.Rotation;hrpsz=a.Character.HumanoidRootPart.Size;end);
-                            local gdata="{Position = Vector3.new("..string.format("%3i, %3i, %3i",hrppos.X,hrppos.Y,hrppos.Z).."), Rotation = Vector3.new("..string.format("%3i, %3i, %3i",hrprot.X,hrprot.Y,hrprot.Z).."), Size = Vector3.new("..tostring(string.format("%3i, %3i, %3i",hrpsz.X,hrpsz.Y,hrpsz.Z))..")}"
+                            local torsopos=CFrame.new(0,0,0);
+                            local torsorot=CFrame.new(0,0,0);
+                            local torsosz=Vector3.new(2,2,1);pcall(function()torsopos=a.Character.Torso.Position;torsorot=a.Character.Torso.Rotation;torsosz=a.Character.Torso.Size;end);
+                            local gdata="{['HumanoidRootPart'] = {Position = Vector3.new("..string.format("%3i, %3i, %3i",hrppos.X,hrppos.Y,hrppos.Z).."), Rotation = Vector3.new("..string.format("%3i, %3i, %3i",hrprot.X,hrprot.Y,hrprot.Z).."), Size = Vector3.new("..tostring(string.format("%3i, %3i, %3i",hrpsz.X,hrpsz.Y,hrpsz.Z))..")}, ['Torso'] = {Position = Vector3.new("..string.format("%3i, %3i, %3i",torsopos.X,torsopos.Y,torsopos.Z).."), Rotation = Vector3.new("..string.format("%3i, %3i, %3i",torsorot.X,torsorot.Y,torsorot.Z).."), Size = Vector3.new("..tostring(string.format("%3i, %3i, %3i",torsosz.X,torsosz.Y,torsosz.Z))..")}}"
                             if(lastdatas[a.Name]~=gdata)then 
                                 lastdatas[a.Name]=gdata;
-                                print(gdata);
-                                socket:Send("data´lagviz´"..a.Name.."´"..game:GetService("Players").LocalPlayer.Name.."´"..items.."´"..data);
+                                socket:Send("data´lagviz´"..a.Name.."´"..game:GetService("Players").LocalPlayer.Name.."´"..getgenv().vizitems.."´"..data);
                             end;
                         elseif(a~=nil)and(a.Name==game:GetService("Players").LocalPlayer.Name)then 
                             table.remove(currentUsers,b);
