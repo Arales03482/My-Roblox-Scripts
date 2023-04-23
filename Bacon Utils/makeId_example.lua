a1830ffc5de34fcaa6f17eb3fe6dea6a50725dbc
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-04-23 13:54:14 +0200

    Update makeId_example.lua

--- Bacon Utils/makeId_example.lua
+++ Bacon Utils/makeId_example.lua
@@ -1,8 +1,8 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Bacon42069/My-Roblox-Scripts/main/Bacon%20Utils/src.lua",true))();
+local t=loadstring(game:HttpGet("https://raw.githubusercontent.com/Bacon42069/My-Roblox-Scripts/main/Bacon%20Utils/src.lua",true))();
 
 local amount=1028;
 local characters={"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","9"};
 local spereratorFreq=8;
 local spererator="-";
-local Password=getgenv().baconUtils.makeId(amount,characters,spereratorFreq,spererator);
+local Password=t.makeId(amount,characters,spereratorFreq,spererator);
 print(Password);
