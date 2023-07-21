82e17064f57e8be49876c858134770bd45a95e7b
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-07-21 21:13:21 +0200

    Create SB Auto Farm Slapples.lua

---
+++ SB Auto Farm Slapples.lua
@@ -0,0 +1,55 @@
+local queueteleport=((syn)and(syn.queue_on_teleport))or(queue_on_teleport)or((fluxus)and(fluxus.queue_on_teleport))
+if(queueteleport)then 
+	game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(state)
+		if(state==Enum.TeleportState.Started)then 
+			queueteleport("loadstring(game:HttpGet('put_code_url_here'))()")
+		end;
+	end);
+end;
+
+spawn(function()
+	while(wait())do 
+		pcall(function()
+			firetouchinterest(game:GetService("Workspace").Lobby.Teleport1,game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,0);
+			firetouchinterest(game:GetService("Workspace").Lobby.Teleport1,game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,1);
+			for _,slapple in pairs(game:GetService("Workspace").Arena.island5.Slapples:GetChildren())do 
+				if(slapple:FindFirstChild("Glove")~=nil)and(slapple:FindFirstChild("Glove").Transparency<=0.2)and(slapple:FindFirstChild("Glove"):FindFirstChildWhichIsA("TouchTransmitter")~=nil)then 
+					firetouchinterest(slapple:FindFirstChild("Glove"),game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,0);
+					firetouchinterest(slapple:FindFirstChild("Glove"),game:GetService("Players").LocalPlayer.Character.HumanoidRootPart,1);
+				end;
+			end;
+		end);
+	end;
+end);
+
+spawn(function()
+	while(wait(5))do 
+		xpcall(function()
+			local servers = {}
+            local cursor = nil
+            repeat
+                local fullurl = string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100", game.PlaceId)
+                if cursor then
+                    fullurl = fullurl .."&cursor=".. cursor
+                end
+
+                local req = httprequest({Url = fullurl})
+                local body = game:GetService("HttpService"):JSONDecode(req.Body)
+                cursor = body.nextPageCursor
+                if body and body.data then
+                    for i, v in next, body.data do
+                        if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                            table.insert(servers, v.id)
+                            --warn(v.playing, v.maxPlayers, v.id)
+                        end
+                    end
+                end
+                print(#servers)
+                if #servers >= 1 then
+                    break
+                end
+            until not body.data or not body.data[1]
+			game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, servers[math.random(1, #servers)], game:GetService("Players").LocalPlayer)
+		end,warn);
+	end;
+end);
