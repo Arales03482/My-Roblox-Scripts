da71eb6cc06e957d9349b3bc4fb4276f26f8e4da
Bacon <69711892+Bacon42069@users.noreply.github.com>
2023-08-28 00:12:25 +0200

    Update Fling Kit.lua

--- Fling Kit.lua
+++ Fling Kit.lua
@@ -6,6 +6,17 @@ getgenv().FlingSwimFlySpeed=30;
 getgenv().TouchFling=false;
 getgenv().TouchFlingStrength=-20000;
 getgenv().TouchFlingShouldSwim=false;
+getgenv().FlipCharacter=false;
+getgenv().FlipCharacterX=0;
+getgenv().FlipCharacterY=0;
+getgenv().FlipCharacterZ=0;
+getgenv().FlipCharacterRotX=0;
+getgenv().FlipCharacterRotY=0;
+getgenv().FlipCharacterRotZ=180;
+getgenv().UsePreAnimation=false;
+getgenv().GoIntoGround=false;
+getgenv().AntiLock=false;
+getgenv().AntiAntiLock=false;
 getgenv().PlayerNoclip=false;
 
 --anti afk
@@ -25,7 +36,8 @@ b:Toggle("Fling Swim",function(a)
 		    game:GetService("Workspace").Gravity=0;
             local char=game:GetService("Players").LocalPlayer.Character;
             while(getgenv().FlingSwim==true)and(char~=nil)and(char:IsDescendantOf(game)==true)do 
-                local dt=game:GetService("RunService").Heartbeat:Wait();
+                local mode=((getgenv().UsePreAnimation==true)and("PreAnimation"))or("PreRender");
+                local dt=game:GetService("RunService")[mode]:Wait();
                 xpcall(function()
                     for _,a in pairs(char:GetDescendants())do 
                         if(a:IsA("BasePart"))then 
@@ -112,9 +124,10 @@ b:Toggle("Touch Fling",function(a)
         local rnd=Random.new(tick());
         while(getgenv().TouchFling==true)and(game:GetService("RunService").PostSimulation:Wait())do 
             pcall(function()
+                local mode=((getgenv().UsePreAnimation==true)and("PreAnimation"))or("PreRender");
                 local PreVelocity=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity;
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity=Vector3.new(0,rnd:NextNumber(1,getgenv().TouchFlingStrength),0);
-                game:GetService("RunService").PreRender:Wait();
+                game:GetService("RunService")[mode]:Wait();
                 game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity=PreVelocity;
             end);
         end;
@@ -139,6 +152,108 @@ end);
 
 b:Toggle("Touch Fling Should Swim",function(a)
     getgenv().TouchFlingShouldSwim=a;
+end);
+
+b:Toggle("Flip Character",function(a)
+    getgenv().FlipCharacter=a;
+    spawn(function()
+        while(getgenv().FlipCharacter==true)and(game:GetService("RunService").PostSimulation:Wait())do 
+            pcall(function()
+                local mode=((getgenv().UsePreAnimation==true)and("PreAnimation"))or("PreRender");
+                local PreCFrame=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame;
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=PreCFrame*CFrame.new(getgenv().FlipCharacterX,getgenv().FlipCharacterY,getgenv().FlipCharacterZ)*CFrame.Angles(math.rad(getgenv().FlipCharacterRotX),math.rad(getgenv().FlipCharacterRotY),math.rad(getgenv().FlipCharacterRotZ));
+                game:GetService("RunService")[mode]:Wait();
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=PreCFrame;
+            end);
+        end;
+    end);
+end);
+
+b:Box("Flip Character X","number",function(a)
+    getgenv().FlipCharacterX=(tonumber(a))or(getgenv().FlipCharacterX);
+end);
+
+b:Box("Flip Character Y","number",function(a)
+    getgenv().FlipCharacterY=(tonumber(a))or(getgenv().FlipCharacterY);
+end);
+
+b:Box("Flip Character Z","number",function(a)
+    getgenv().FlipCharacterZ=(tonumber(a))or(getgenv().FlipCharacterZ);
+end);
+
+b:Box("Flip Character Rot X","number",function(a)
+    getgenv().FlipCharacterRotX=(tonumber(a))or(getgenv().FlipCharacterRotX);
+end);
+
+b:Box("Flip Character Rot Y","number",function(a)
+    getgenv().FlipCharacterRotY=(tonumber(a))or(getgenv().FlipCharacterRotY);
+end);
+
+b:Box("Flip Character Rot Z","number",function(a)
+    getgenv().FlipCharacterRotZ=(tonumber(a))or(getgenv().FlipCharacterRotZ);
+end);
+
+b:Toggle("Use Pre Animation",function(a)
+    getgenv().UsePreAnimation=a;
+end);
+
+b:Toggle("Go Into Ground",function(a)
+    getgenv().GoIntoGround=a;
+    spawn(function()
+        local part=Instance.new("Part");
+        part.Transparency=1;
+        part.CanCollide=false;
+        part.Anchored=true;
+        part.Parent=game:GetService("JointsService");
+        while(getgenv().GoIntoGround==true)and(game:GetService("RunService").PostSimulation:Wait())do 
+            xpcall(function()
+                local PreCFrame=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame;
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=PreCFrame*CFrame.new(0,-10,0);
+                game:GetService("RunService").PreRender:Wait();
+                local offset=game:GetService("Players").LocalPlayer.Character.Humanoid.CameraOffset+Vector3.new(0,1.5,0);
+                local p=PreCFrame.Position;
+                local look=CFrame.lookAt(p,p+Vector3.new(game:GetService("Workspace").CurrentCamera.CFrame.LookVector.X*4,0,game:GetService("Workspace").CurrentCamera.CFrame.LookVector.Z*4));
+                part.CFrame=look*CFrame.new(offset)*CFrame.Angles(math.rad(30),0,0);
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame=look;
+                game:GetService("Workspace").CurrentCamera.CameraSubject=part;
+            end,warn);
+        end;
+        part:Destroy();
+        game:GetService("Workspace").CurrentCamera.CameraSubject=game:GetService("Players").LocalPlayer.Character.Humanoid;
+    end);
+end);
+
+b:Toggle("Anti Lock",function(a)
+    getgenv().AntiLock=a;
+    spawn(function()
+        local rnd=Random.new(tick());
+        while(getgenv().AntiLock==true)and(game:GetService("RunService").PostSimulation:Wait())do 
+            pcall(function()
+                local mode=((getgenv().UsePreAnimation==true)and("PreAnimation"))or("PreRender");
+                local PreVelocity=game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity;
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity=Vector3.new(500,0,0);
+                game:GetService("RunService")[mode]:Wait();
+                game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Velocity=PreVelocity;
+            end);
+        end;
+    end);
+end);
+
+b:Toggle("Anti Anti Lock",function(a)
+    getgenv().AntiAntiLock=a;
+    spawn(function()
+        while(getgenv().AntiAntiLock==true)and(game:GetService("RunService").PostSimulation:Wait())do 
+            pcall(function()
+                for _,p in pairs(game.Players:GetChildren())do 
+                    if(p.Name~=game.Players.LocalPlayer.Name)then 
+                        local hrp=p.Character.HumanoidRootPart;
+                        hrp.Velocity=Vector3.new(hrp.Velocity.X,0,hrp.Velocity.Z);
+                        hrp.AssemblyLinearVelocity=Vector3.new(hrp.Velocity.X,0,hrp.Velocity.Z);
+                    end;
+                end;
+            end);
+        end;
+    end);
 end);
 
 b:Toggle("Player Noclip",function(a)
