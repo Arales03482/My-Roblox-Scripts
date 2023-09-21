119829ef24c673db970680099c0bb6c705d3aabf
Bacon <69711892+Arales03482@users.noreply.github.com>
2023-09-21 22:52:56 +0200

    Update TPRR AutoFarming.lua

--- TPRR AutoFarming.lua
+++ TPRR AutoFarming.lua
@@ -8,7 +8,7 @@ getgenv().AutoSetTime=false;
 getgenv().AutoSetTimeNumber=6;
 
 --Anti AFK
-if(getgenv().kuefg834rjiy983450==nil)then game:GetService("Players").LocalPlayer.Idled:connect(function()game:service("VirtualUser"):CaptureController();game:service("VirtualUser"):ClickButton2(Vector2.new());end);getgenv().kuefg834rjiy983450="nope not cracking this bitch today";end;
+if(getgenv().kuefg834rjiy983450~=nil)and(typeof(getgenv().kuefg834rjiy983450)=="RBXScriptConnection")then getgenv().kuefg834rjiy983450:Disconnect();end;getgenv().kuefg834rjiy983450=game:GetService("Players").LocalPlayer.Idled:Connect(function()game:service("VirtualUser"):CaptureController();game:service("VirtualUser"):ClickButton2(Vector2.new(0,0));end);
 
 -- Main Script
 b:Toggle("Auto Collect Tapes",function(a)
@@ -94,4 +94,69 @@ end);
 c:Box("Auto Set Time Amount","number",function(a)
     getgenv().AutoSetTimeNumber=a;
     print(a);
+end);
+
+b:Button("Server Hop",function(a)
+    local queue_teleport=((syn)and(syn.queue_on_teleport))or(queue_on_teleport)or((fluxus)and(fluxus.queue_on_teleport));
+    game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(status)
+        if(status==Enum.TeleportState.InProgress)and(queue_teleport~=nil)then 
+            queue_teleport("wait(0.5);if(game:IsLoaded()==false)then game.Loaded:Wait();end;loadstring(game:HttpGet(\"https://raw.githubusercontent.com/Arales03482/My-Roblox-Scripts/main/TPRR%20AutoFarming.lua\"))();");
+        end;
+    end);
+    spawn(function()
+        local fullurl=string.format("https://games.roblox.com/v1/games/%d/servers/Public?sortOrder=Desc&limit=100",game.PlaceId);
+        local url_data={Url=fullurl};
+        local cursor=nil;
+        local servers={};
+        while(wait())do 
+            xpcall(function()
+                while(wait())do 
+                    table.clear(servers);
+                    url_data.Url=((cursor~=nil)and(fullurl.."&cursor="..cursor))or(fullurl);
+    
+                    local req=requester(url_data);
+                    local body=game:GetService("HttpService"):JSONDecode(req.Body);
+                    if(body==nil)or(body.data==nil)or(body.data[1]==nil)then 
+                        cursor=nil;
+                        table.clear(body);
+                        body=nil;
+                        break;
+                    end;
+                    local data=body.data;
+                    if(cursor==body.nextPageCursor)and(cursor~=nil)then 
+                        cursor=nil;
+                        table.clear(body);
+                        body=nil;
+                        break;
+                    end;
+                    cursor=body.nextPageCursor;
+                    if(body~=nil)and(data~=nil)then 
+                        for _,server_data in next,data do 
+                            if(type(server_data)=="table")and(tonumber(server_data.playing)~=nil)and(tonumber(server_data.maxPlayers)~=nil)and(server_data.playing<server_data.maxPlayers)and(server_data.id~=game.JobId)and(table.find(getgenv().last_servers,server_data.id)==nil)then 
+                                servers[#servers+1]=server_data.id;
+                            end;
+                            table.clear(server_data);
+                            server_data=nil;
+                        end;
+                        table.clear(data);
+                        table.clear(body);
+                        data=nil;
+                        body=nil;
+                    end;
+                    print(#servers);
+                    if(#servers>=1)then 
+                        break;
+                    end;
+                    table.clear(servers);
+                end;
+                if(#servers>=1)then 
+                    local serverid=servers[math.random(1,#servers)];
+                    table.clear(servers);
+                    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId,serverid,game:GetService("Players").LocalPlayer);
+                    game:GetService("Players").LocalPlayer.OnTeleport:Wait();
+                end;
+                table.clear(servers);
+            end,warn);
+        end;
+    end);
 end);
